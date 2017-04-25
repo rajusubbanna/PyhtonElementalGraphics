170917e94325358121be80e2e45163eda69b35d0
rajusubbanna <rajusubbanna@gmail.com>
2017-04-25 11:20:04 -0700

    Example-0

---
+++ TimeForBreak.py
@@ -0,0 +1,12 @@
+#This is to demonstrate import, use it and sys commands... very basic
+#Let me know if you have any questions
+
+import time
+import webbrowser
+import time;
+print("This program started on "+time.ctime())
+for  i in range(3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=djnqoEO2rLc")
+    
+    
